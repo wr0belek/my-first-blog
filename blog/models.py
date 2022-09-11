3495ac89501be8a75e430710d40ea0118457a448
wr0belek <mal07gosia@gmail.com>
2022-09-11 19:28:50 +0200

    HTML z towarzystwem django

--- blog/models.py
+++ blog/models.py
@@ -6,11 +6,11 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateField(default = timezone.now)
+    created_date = models.DateField(default = timezone.now())
     published_date = models.DateField(blank=True, null=True)
     
     def publish(self):
-        self.published_date = timezone.now
+        self.published_date = timezone.now()
         self.save()
     
     def __str__(self):
